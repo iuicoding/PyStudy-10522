639b6b9c90c2be46e575a7e2a4e984d2a72efebc
Salmon0220 <chanlee0728@gmail.con>
2022-06-22 15:38:29 +0900

    update

---
+++ pystudy_2/6교시.py
@@ -0,0 +1,21 @@
+alist = [1, 2, 3, 4, 5]
+
+print(alist)
+
+if 5 in alist:
+    print("5가 있넹")
+
+if 232 not in alist:
+    print("232 그딴거는 없어 이눔아")
+
+
+print(alist[2])
+
+a = [1,2,3,4]
+b = [7,8,9,10,11,12]
+print(a+b)
+print(len(b))
+
+yourface = [4]
+print(b * 5)
+
