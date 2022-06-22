e6c9b7ff07fa75f2d59c25af2fbf2a984f2a1b40
Salmon0220 <chanlee0728@gmail.con>
2022-06-22 15:10:49 +0900

    update

---
+++ pystudy_2/이찬.py
@@ -0,0 +1,13 @@
+score = float(input(">살고싶으면 점수를 입력해라 이놈아: "))
+if score == 100:
+    print("좀 치네")
+if 70 <= score < 100:
+    print("ㄲㅂ")
+if 50 <= score < 70:
+    print("분발해라")
+if 30 <= score < 50:
+    print("접어라")
+if 1 <= score < 30:
+    print("...허접")
+
+
